7cac5558801e9164998ee2e7491102d50791e956
Pritam <pritamkundu.pk5@gmail.com>
2021-09-19 22:45:28 +0600

    Automate_Dial_Number_Android_Mobile !!

---
+++ src/Automate_Dial_Number_Android_Mobile.java
@@ -0,0 +1,54 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+//import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class Automate_Dial_Number_Android_Mobile {
+
+	public static void main(String[] args) throws MalformedURLException, InterruptedException {
+		DesiredCapabilities Dc = new DesiredCapabilities();
+		Dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+		Dc.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		Dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.0");
+		Dc.setCapability(MobileCapabilityType.DEVICE_NAME, "Pritam");
+		
+		//Install the Apps
+		//Dc.setCapability(MobileCapabilityType.APP, "C:\\Users\\PRITAM\\Downloads\\apkinfo.apk");
+	    
+		//Launch the Calculator Apps
+		Dc.setCapability("appPackage", "com.android.dialer");
+	    Dc.setCapability("appActivity", "com.android.dialer.app.DialtactsActivity");
+		
+	    URL url = new URL("http://localhost:4723/wd/hub");
+		AndroidDriver<WebElement> driver = new AndroidDriver<WebElement>(url,Dc);
+	    
+		WebElement dialer = driver.findElementById("com.android.dialer:id/fab");
+		dialer.click(); //dialer Button
+		
+		//Dial 01743739440
+		driver.findElementById("com.android.dialer:id/zero").click(); //Zero
+		driver.findElementById("com.android.dialer:id/one").click(); //One
+		driver.findElementById("com.android.dialer:id/seven").click(); //seven
+		driver.findElementById("com.android.dialer:id/four").click(); //four
+		driver.findElementById("com.android.dialer:id/three").click(); //three
+		driver.findElementById("com.android.dialer:id/seven").click(); //seven
+		driver.findElementById("com.android.dialer:id/three").click(); //three
+		driver.findElementById("com.android.dialer:id/nine").click(); //nine
+		driver.findElementById("com.android.dialer:id/four").click(); //four
+		driver.findElementById("com.android.dialer:id/four").click(); //four
+		driver.findElementById("com.android.dialer:id/zero").click(); //Zero
+		Thread.sleep(4000);
+		
+		//Dial Action button
+		driver.findElementById("com.android.dialer:id/dialpad_floating_action_button").click();
+		
+		Thread.sleep(4000);
+
+	}
+
+}
